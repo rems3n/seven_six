e12d0c5f0e95acc25035e8aba575b80197b8d186
remsen <cgusty1@gmail.com>
2023-12-27 14:50:28 -0500

    Created Deck object

---
+++ deck.js
@@ -0,0 +1,23 @@
+const SUITS = ['♥', '♣', '♦', '♠'];
+const VALUES = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+class Deck {
+    constructor(cards) {
+        this.cards = cards
+    }
+}
+
+class Card {
+    constructor (suit, value) {
+        this.suit = suit
+        this.value = value
+    }
+}
+
+function freshDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit,value)
+        })
+    })
+}
